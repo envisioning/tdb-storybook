c797bc7a0fd6b5020e1b943df81b83c6967864b9
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2016-08-09 21:17:47 -0300

    Style props on Panel & longContent story

--- src/components/Panel/stories.js
+++ src/components/Panel/stories.js
@@ -2,13 +2,46 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import Panel from './';
 
+const longContent = <div>
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+  A<br />
+</div>
+
 storiesOf('Panel', module)
   .add('300x300', () => (
     <Panel
+      style={{width: 300, minHeight: 300}}
       header={ <h2> This is the header </h2> }>
-      <div style={{
-        height: 300,
-        width: 300
-      }}> This is the body </div>
+      <div>
+        This is the body
+      </div>
+
+    </Panel>
+  ))
+  .add('long content', () => (
+    <Panel
+      style={{width: 300, minHeight: 300}}
+      header={ <h2> This is the header </h2> }>
+      <div>
+        {longContent}
+      </div>
+
     </Panel>
   ))
